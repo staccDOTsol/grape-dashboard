813a971dc806d812b2a999cd5c01fcd5755179c5
Kirk <kfournia@gmail.com>
2022-04-04 17:09:37 +0300

    updated with raydium token prices as a backup

--- src/views/Portfolio/Portfolio.jsx
+++ src/views/Portfolio/Portfolio.jsx
@@ -91,6 +91,7 @@ import { FarmsView } from "../";
 export const PortfolioView = () => {
   const [initPortfolio, setInitPortfolio] = useState(null);
   const [initCGPriceData, setInitCGPriceData] = useState(null);
+  const [initRaydiumPriceData, setInitRaydiumPriceData] = useState(null);
   const [initNewPriceData, setInitNewPriceData] = useState(null);
   const [portfolioPositions, setPorfolioBalances] = useState(null);
   const [tokenMap, setTokenMap] = useState(new Map());
@@ -278,6 +279,23 @@ export const PortfolioView = () => {
     }catch(e){return null;}
   }
 
+    //Get Raydium Prices
+    const fetchRaydiumPriceList = async () => {
+      const response = await fetch("https://api.raydium.io/coin/price", {
+        method: "GET",
+        //body: JSON.stringify(body),
+        headers: { "Content-Type": "application/json" },
+      }).catch((error)=>{
+        console.log("ERROR GETTING RAYDIUM DATA!");
+        return null;
+      });
+      
+      try{
+        const json = await response.json();
+        return json;
+      }catch(e){return null;}
+    }
+
   //Get Prices RPC
   const fetchCoinGeckoPriceList = async () => {
     const response = await fetch("https://api.coingecko.com/api/v3/simple/price?include_24hr_change=true&ids=solana,kitty-solana,astrapad,buff-samo,solana-inu,wipemyass,almond,swole-doge,oogi,solex-finance,bamboo-coin,solum,solberg,sola-token,solana,usd-coin,everid,impossible-finance,investin,bitcoin,ethereum,yearn-finance,chainlink,ripple,tether,sushi,aleph,swipe,hedget,cream-2,upbots,helium,frontier-token,akropolis,hxro,uniswap,serum,ftx-token,megaserum,usd-coin,tomochain,karma-dao,lua-token,math,keep-network,swag-finance,celsius-degree-token,reserve-rights-token,1inch,the-graph,compound-coin,pax-gold,strong,bonfida,kin,maps,oxygen,brz,tether,xmark,raydium,bitsong,3x-short-eos-token,3x-long-eos-token,3x-short-bnb-token,3x-long-bnb-token,3x-long-bitcoin-sv-token,3x-short-bitcoin-sv-token,3x-short-litecoin-token,3x-long-litecoin-token,3x-long-bitcoin-token,3x-short-bitcoin-token,3x-short-bitcoin-cash-token,3x-long-bitcoin-cash-token,3x-long-ethereum-token,3x-short-ethereum-token,3x-long-altcoin-index-token,3x-short-altcoin-index-token,3x-long-shitcoin-index-token,3x-short-shitcoin-index-token,3x-long-midcap-index-token,3x-short-midcap-index-token,3x-short-chainlink-token,3x-long-chainlink-token,3x-long-xrp-token,3x-short-xrp-token,1x-long-btc-implied-volatility-token,1x-short-btc-implied-volatility,aave,serum-ecosystem-token,holy-trinity,bilira,3x-long-dogecoin-token,perpetual-protocol,weth,coin-capsule,ftx-token,true-usd,tokenlon,allianceblock,skale,unlend-finance,orion-protocol,sparkpoint,uma,smartkey,mirror-protocol,growth-defi,xdai-stake,yearn-finance,basic-attention-token,basic-attention-token,decentraland,xio,unilayer,unimex-network,1inch,armor,armor-nxm,defipulse-index,deltahub-community,kira-network,energy-web-token,cryptocurrency-top-10-tokens-index,audius,vesper-finance,keep3rv1,lead-token,uniswap,wrapped-bitcoin,union-protocol-governance-token,unisocks,idextools,hex,cream-2,yfimobi,zeroswap,wrapped-anatha,ramp,parsiq,smooth-love-potion,the-sandbox,concentrated-voting-power,republic-protocol,sora,funfair,pickle-finance,pax-gold,quant-network,oraichain-token,truefi,mcdex,nucypher,razor-network,chainlink,unfederalreserve,nusd,hegic,xfinance,dextf,iexec-rlc,cvault-finance,cyberfi,wise-token11,gnosis,poolz-finance,dai,sushi,fyooz,quiverx,unitrade,bird-money,axion,bridge-mutual,dynamite,bitberry-token,waxe,matic-network,robonomics-network,aave,ethlend,polkastarter,unibright,dia-data,frax,keep-network,reserve-rights-token,88mph,paid-network,swipe,request-network,whale,kleros,krown,apy-finance,ocean-protocol,shopping-io,binance-wrapped-btc,unistake,maker,harvest-finance,usd-coin,aragon,pundi-x,redfox-labs-2,meta,rubic,noia-network,celsius-degree-token,crowns,option-room,yield-optimization-platform,lgcy-network,rio-defi,mahadao,rocket-pool,nexo,saffron-finance,stabilize,balancer,band-protocol,swapfolio,loopring,perpetual-protocol,compound-governance-token,havven,dlp-duck-token,chain-games,the-graph,rootkit,trustswap,terra-virtua-kolect,omisego,wrapped-terra,bondly,dextrust,ampleforth,polkamarkets,curve-dao-token,degenerator,exnetwork-token,tether,yield,kyber-network,coti,injective-protocol,0x,superfarm,ankreth,surf-finance,renbtc,dmm-governance,hermez-network-token,rally-2,yfdai-finance,fractal,axie-infinity,enjincoin,yield-app,duckdaodime,rarible,amp-token,fsw-token,binance-usd,aave-dai-v1,aave-tusd-v1,aave-usdc-v1,aave-usdt-v1,aave-susd-v1,aave-bat-v1,aave-eth-v1,aave-link-v1,aave-knc-v1,aave-mkr-v1,aave-mana-v1,aave-zrx-v1,aave-snx-v1,aave-wbtc-v1,aave-busd-v1,aave-enj-v1,aave-ren-v1,ayfi,aave-usdt,aave-wbtc,aave-zrx,aave-bat,aave-busd,aave-dai,aave-enj,aave-knc,aave-link,aave-mana,aave-mkr,aave-ren,aave-snx,aave-susd,aave-tusd,aave-usdc,stake-dao,cope,cope,mango-market-caps,rope-token,media-network,step-finance,solanium,samoyedcoin,panda-coin,star-atlas,star-atlas-dao,soldoge,synthetify-token,moonlana,solape-token,woof-token,mercurial,lotto,bole-token,apyswap,shibaverse-token,shibaverse-token,solfarm,ardcoin,cheems,cato,ninja-protocol,boring-protocol,dexlab,grape-2,apexit-finance,black-label,orca,renbtc,renbch,rendoge,renzec,sail,aldrin,oxbull-solana,fabric,naxar,space-hamster,gu,liq-protocol,cropperfinance,solrise-finance,jet,cheesesoda-token,only1,terrausd,orbs,solberry,coin98,saber,huobi-btc,husd,hapi,larix,msol,mim,port-finance,jpyc,mango-markets,solanasail-governance-token,parrot-usd,parrot-protocol,sunny-aggregator,cyclos,lido-staked-ether,million,million,shapeshift-fox-token,ashera,balisari,solminter,tether,usd-coin,renfil,bitspawn,socean-staked-sol,agronomist,polyplay,allbridge,binance-usd,weth,tether,usd-coin,multi-collateral-dai,celo-dollar,chihuahuasol,chronologic,ftx-token,matrixetf,solblank,marinade,wrapped-conceal,graviton,dogelana,usd-coin,bitcoin,solana,ftx-token,ethereum,usd-coin,tether,dai,himalayan-cat-coin,frakt-token,tether,usd-coin,multi-collateral-dai,wrapped-bitcoin,avalanche,aurory,matrixetf,lizard-token,cave,ftx-token,terra-usd,weth,serum,terra-luna,husd,binance-usd,frax,huobi-btc,usdk,sushi,uniswap,wbnb,chainlink,pax-gold,hxro,swipe,frax-share,celsius-network-token,cream,usd-coin,wrapped-bitcoin,tether,floof,baby-samo-coin,solend,lunachow,lunachow,biconomy-exchange-token,ariadne,solana,invictus,wrapped-bitcoin,polygon,polygon,binance-usd,the-4th-pillar,genopets,safe-coin-2&vs_currencies=usd",{
@@ -336,7 +354,7 @@ export const PortfolioView = () => {
       
       // first load a skeleton of the portfolio
       // then fetch cg
-      let [cgPriceData] = await Promise.all([fetchCoinGeckoPriceList()]);
+      let [cgPriceData, raydiumPriceData] = await Promise.all([fetchCoinGeckoPriceList(), fetchRaydiumPriceList()]);
 
       // finally check farms (this can break so lets split)
       let [sol, newPriceData] = await Promise.all([fetchSOLBalance(), fetchNSWPriceList()]);
@@ -344,6 +362,7 @@ export const PortfolioView = () => {
       setInitNewPriceData(newPriceData);
       setInitPortfolio(portfolio);
       setInitCGPriceData(cgPriceData);
+      setInitRaydiumPriceData(raydiumPriceData);
       setTokenMap(tokenMap);
       
       // we need to now first get portfolio 
@@ -401,15 +420,19 @@ export const PortfolioView = () => {
         let price = (tmapitem && tmapitem.extensions?.coingeckoId) && cgPriceData[tmapitem.extensions.coingeckoId]?.usd || 0;
         let usd_24h_change = (tmapitem && tmapitem.extensions?.coingeckoId) && cgPriceData[tmapitem.extensions.coingeckoId]?.usd_24h_change || '';
 
-        // tokenInfo.extensions.coingeckoId
         if (tmapitem){  
           if (tmapitem.extensions?.coingeckoId){
 
-            //try{
-              //console.log("tmapitem: "+JSON.stringify(tmapitem));
-              //console.log("pushing ("+tmapitem.extensions.coingeckoId+"): " + JSON.stringify(cgPriceData[tmapitem.extensions.coingeckoId]?.usd));
-              //console.log("FOUND: "+psymbol);
-              // IMPORTANT ADD CHECK TO GET PRICE FROM CG
+            // handle prices not on CG
+            if (price === 0){ // check raydium pool data
+              if ((tmapitem) && (tmapitem.symbol)) {
+                try{
+                  if (raydiumPriceData[tmapitem.symbol.toUpperCase()])
+                    price = raydiumPriceData[tmapitem.symbol.toUpperCase()];
+                }catch(e){console.log("ERR:"+e)}
+              }
+             }
+
               priceData.push({
                 address: token.pubkey,
                 decimals: token.decimals,
